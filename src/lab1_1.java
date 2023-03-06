1f09197a341b469d64ff58ea1eefe433f1e25b60
staniekk <jstaniekk@gmail.com>
2023-03-06 10:57:01 +0100

    lab1

---
+++ src/lab1_1.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+// Glowna klasa
+public class lab1_1{
+    public static void main(String[] args){
+    JFrame f = new JFrame();
+    f.setSize(320, 200);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    f.setVisible(true);
+    }
+}
